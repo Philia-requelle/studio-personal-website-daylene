8ff2ce279137be0af7d0a901937e7f3df004a3a3
Gloriaza Odyardy <gloriazalawrance@gmail.com>
2025-07-21 23:07:42 +0700

    initial commit

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,2 @@
-export const schemaTypes = []
+import {portofolioType} from './portofolioTypes'
+export const schemaTypes = [portofolioType]
---
+++ schemaTypes/portofolioTypes.ts
@@ -0,0 +1,63 @@
+import { defineField, defineType } from 'sanity'
+
+export const portofolioType = defineType({
+  name: 'portofolio',
+  title: 'Portofolio',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'summary',
+      title: 'Short Description',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'role',
+      title: 'Role',
+      type: 'string',
+    }),
+    defineField({
+      name: 'contribution',
+      title: 'Contribution',
+      type: 'string',
+    }),
+    defineField({
+      name: 'year',
+      title: 'Year',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Full Description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'gallery',
+      title: 'Gallery',
+      type: 'array',
+      of: [
+        {
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+        },
+      ],
+    }),
+  ],
+})
